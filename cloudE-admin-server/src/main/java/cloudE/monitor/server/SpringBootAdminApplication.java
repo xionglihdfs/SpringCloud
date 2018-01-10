3ccc0ab5dcb90f719ac6e34154617b691cbda1db
xiongli <xionglihdfs@163.com>
2018-01-10 11:04:28 +0800

    add spring cloud code.

---
+++ cloudE-admin-server/src/main/java/cloudE/monitor/server/SpringBootAdminApplication.java
@@ -0,0 +1,21 @@
+
+package cloudE.monitor.server;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xionglihdfs
+ * @Description: 服务监控
+ * @date 2017年12月24日
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
